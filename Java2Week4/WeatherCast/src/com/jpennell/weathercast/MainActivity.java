b7f50c058058ad367d0b995e83956a2f3d52aec9
Jerry Pennell <jwpenn@fullsail.edu>
2013-08-27 22:41:13 -0400

    Refactoring Main activity

--- Java2Week4/WeatherCast/src/com/jpennell/weathercast/MainActivity.java
+++ Java2Week4/WeatherCast/src/com/jpennell/weathercast/MainActivity.java
@@ -22,14 +22,13 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Messenger;
 import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -38,22 +37,26 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.jpennell.library.DetailFragment;
 import com.jpennell.library.FileSystem;
-import com.jpennell.library.Singleton;
+import com.jpennell.library.FormFragment;
 import com.jpennell.library.StorageParser;
 import com.jpennell.library.WeatherContentProvider;
 import com.jpennell.library.Web;
-
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class MainActivity.
  */
-public class MainActivity extends SherlockActivity {
+public class MainActivity extends SherlockFragmentActivity implements FormFragment.formListener{
 
-
+    //Global variables
+    /** The _context. */
+    Context _context = this;
+    
     /** The _is connected. */
     Boolean _isConnected = false;
     
@@ -63,92 +66,24 @@ public class MainActivity extends SherlockActivity {
     /** The Constant DAY_PICKED_REQUEST. */
     static final int DAY_PICKED_REQUEST = 1;
 
-    /**
-     * Creates the layout.
-     */
-    @SuppressLint("HandlerLeak")
-	public void createLayout() {
-    	
-    	//Setting Context from singleton class
-    	Singleton.getInstance().setContext(this);
 
-        // Add Search Handler
+    /**
+     * Handle service connection.
+     */
+    public void handleServiceConnection() {
         Button searchButton = (Button) findViewById(R.id.searchButton);
 
-        // Create onClickListener for searchButton
-        searchButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                final EditText field = (EditText) findViewById(R.id.searchField);
-                Log.i("CLICK HANDLER", field.getText().toString());
-
-                Handler myHandler = new Handler() {
-                    @Override
-                    public void handleMessage(Message message) {
-                        if (message.arg1 == RESULT_OK && message.obj != null) {
-                            String confirmedURL = message.obj.toString();
-                            Log.i("URL RESPONSE", confirmedURL);
-
-                            try {
-                                // Pull JSON data from API
-                                JSONObject json = new JSONObject(confirmedURL);
-                                JSONObject data = json.getJSONObject("data");
-                                Boolean error = data.has("error");
-                                if (error) {
-                                    // Create toast (popup)
-                                    Toast toast = Toast.makeText(Singleton.getInstance().getContext(),"Invalid location", Toast.LENGTH_SHORT);
-                                    toast.show();
-                                } else {
-                                    // Get JSON data to determine correct zip code
-                                    String request = data.getJSONArray("request").getJSONObject(0).getString("query");
-
-                                    // Create toast (popup)
-                                    Toast toast = Toast.makeText(Singleton.getInstance().getContext(),"Valid location, " + request, Toast.LENGTH_SHORT);
-                                    toast.show();
-
-                                    // Query content provider
-                                    Cursor cursor = getContentResolver().query(WeatherContentProvider.WeatherData.CONTENT_URI, null, null, null, null);
-
-                                    // Call displayCurrent method
-                                    displayCurrent();
-
-                                    // Call displayFiveDay method
-                                    displayFiveDay(cursor);
-                                }
-                            } catch (JSONException e) {
-                                Log.e("JSON", e.toString());
-                            }
-                        }
-                    }
-                };
-                // Create Messenger Class
-                Messenger myMessenger = new Messenger(myHandler);
-
-                Intent intent = new Intent(Singleton.getInstance().getContext(),WeatherService.class);
-                intent.putExtra("messenger", myMessenger);
-                intent.putExtra("zip", field.getText().toString());
-
-                // Start Intent
-                startService(intent);
-                
-                //Close out Keyboard
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            	imm.hideSoftInputFromWindow(field.getWindowToken(), 0);
-            }
-        });
-
         // Detect Network Connection
-        _isConnected = Web.getConnectionStatus(Singleton.getInstance().getContext());
+        _isConnected = Web.getConnectionStatus(_context);
         if (_isConnected) {
-            Log.i("NETWORK CONNECTION", Web.getConnectionType(Singleton.getInstance().getContext()));
+            Log.i("NETWORK CONNECTION", Web.getConnectionType(_context));
 
             // Enable button
             searchButton.setClickable(true);
 
         } else {
             // Alert if not connected
-            AlertDialog.Builder alert = new AlertDialog.Builder(Singleton.getInstance().getContext());
+            AlertDialog.Builder alert = new AlertDialog.Builder(_context);
             alert.setTitle("No Network Connection");
             alert.setMessage("Please check your network connections and try again.");
             alert.setCancelable(false);
@@ -163,7 +98,6 @@ public class MainActivity extends SherlockActivity {
             // Disable button
             searchButton.setClickable(false);
         }
-      
     }
 
 
@@ -172,7 +106,7 @@ public class MainActivity extends SherlockActivity {
      */
     public void displayCurrent() {
         // Display current condition via parsing stored json string
-        String readStorage = FileSystem.readStringFile(Singleton.getInstance().getContext(), "temp", false);
+        String readStorage = FileSystem.readStringFile(_context, "temp", false);
 
         JSONObject json;
         JSONObject data;
@@ -192,7 +126,7 @@ public class MainActivity extends SherlockActivity {
 
             // Convert description text with image and set current condition description image
             ((ImageView)findViewById(R.id.imageView)).setImageResource(StorageParser.getDescImage(ccDesc));
-            ((TextView) findViewById(R.id.textView)).setText(ccTemp + " ¼F");
+            ((TextView) findViewById(R.id.textView)).setText(ccTemp + " F");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -225,74 +159,70 @@ public class MainActivity extends SherlockActivity {
                 dataList.add(displayMap);
             }
         }
-        SimpleAdapter adapter = new SimpleAdapter(Singleton.getInstance().getContext(), dataList, R.layout.list_row, new String[] {"desc", "hi", "low"}, new int[] {R.id.desc, R.id.tempLow, R.id.tempHi});
+        SimpleAdapter adapter = new SimpleAdapter(_context, dataList, R.layout.list_row, new String[] {"desc", "hi", "low"}, new int[] {R.id.desc, R.id.tempLow, R.id.tempHi});
 
         // Set adapter to listView
         _listView.setAdapter(adapter);
-
-        // Call selectDay method to allow the user to click a row
-        selectDay();
     }
-
-
-    /**
-     * Select day.
-     */
-    private void selectDay() {
-        _listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.i("ROW CLICKED", "Row " + i + " clicked");
-
-                @SuppressWarnings("unchecked")
-				HashMap<String, String> detailMap = (HashMap<String, String>) _listView.getItemAtPosition(i);
-
-                Intent detailIntent = new Intent(Singleton.getInstance().getContext(), DetailsActivity.class);
-                // Put extra
-                detailIntent.putExtra("detailDate", detailMap.get("date"));
-                detailIntent.putExtra("detailDesc", detailMap.get("desc"));
-                detailIntent.putExtra("detailHi", detailMap.get("hi"));
-                detailIntent.putExtra("detailLow", detailMap.get("low"));
-                detailIntent.putExtra("detailWind", detailMap.get("wind"));
-
-                Log.i("INTENT", "Works");
-
-                // Start Activity for results
-                startActivityForResult(detailIntent,DAY_PICKED_REQUEST);
-            }
-        });
-    }
-
 
     /* (non-Javadoc)
-     * @see android.app.Activity#onCreate(android.os.Bundle)
+     * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Set content from XML layout
-        setContentView(R.layout.form);
+        setContentView(R.layout.form_frag);
         _listView = (ListView) this.findViewById(R.id.list);
         View listHeader = this.getLayoutInflater().inflate(R.layout.list_header, null);
         _listView.addHeaderView(listHeader);
 
         // Call createLayout method
-        createLayout();
+        //createLayout();
+
+        // Call handleServiceConnection method
+        handleServiceConnection();
     }
-    
+
+
     /* (non-Javadoc)
-     * @see com.actionbarsherlock.app.SherlockActivity#onCreateOptionsMenu(android.view.Menu)
+     * @see com.actionbarsherlock.app.SherlockFragmentActivity#onCreateOptionsMenu(android.view.Menu)
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getSupportMenuInflater().inflate(R.menu.main, menu);
+        //This functionality has to be moved to the fragment
+        //getSupportMenuInflater().inflate(R.menu.main, menu);
     return true;
     }
 
 
     /* (non-Javadoc)
-     * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
+     * @see com.actionbarsherlock.app.SherlockFragmentActivity#onOptionsItemSelected(android.view.MenuItem)
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        Log.i("item ID : ", "onOptionsItemSelected Item ID" + id);
+
+        switch (item.getItemId()) {
+            case 0:
+                Log.i("ACTION BAR", "Web navigation pressed");
+
+                Intent implicitIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.worldweatheronline.com"));
+
+                startActivity(implicitIntent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    /* (non-Javadoc)
+     * @see android.support.v4.app.FragmentActivity#onActivityResult(int, int, android.content.Intent)
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -304,11 +234,121 @@ public class MainActivity extends SherlockActivity {
                 if (result != null) {
                     String date = result.getString("date");
 
-                    Toast toast = Toast.makeText(Singleton.getInstance().getContext(), "You checked the weather for " + date, Toast.LENGTH_SHORT);
+                    Toast toast = Toast.makeText(_context, "You checked the weather for " + date, Toast.LENGTH_SHORT);
                     toast.show();
                 }
             }
         }
     }
 
+
+    // FormFragment methods
+    /* (non-Javadoc)
+     * @see com.jpennell.library.FormFragment.formListener#onWeatherSearch()
+     */
+    @SuppressLint("HandlerLeak")
+	@Override
+    public void onWeatherSearch() {
+        final EditText field = (EditText) findViewById(R.id.searchField);
+        Log.i("CLICK HANDLER", field.getText().toString());
+
+        Handler myHandler = new Handler() {
+            @Override
+            public void handleMessage(Message message) {
+                if (message.arg1 == RESULT_OK && message.obj != null) {
+                    String confirmedURL = message.obj.toString();
+                    Log.i("URL RESPONSE", confirmedURL);
+
+                    try {
+                        // Pull JSON data from API
+                        JSONObject json = new JSONObject(confirmedURL);
+                        JSONObject data = json.getJSONObject("data");
+                        Boolean error = data.has("error");
+                        if (error) {
+                            // Create toast (popup)
+                            Toast toast = Toast.makeText(_context,"Invalid location", Toast.LENGTH_SHORT);
+                            toast.show();
+                        } else {
+                            // Get JSON data to determine correct zip code
+                            String request = data.getJSONArray("request").getJSONObject(0).getString("query");
+
+                            // Create toast (popup)
+                            Toast toast = Toast.makeText(_context,"Valid location, " + request, Toast.LENGTH_SHORT);
+                            toast.show();
+
+                            // Query content provider
+                            Cursor cursor = getContentResolver().query(WeatherContentProvider.WeatherData.CONTENT_URI, null, null, null, null);
+
+                            // Call displayCurrent method
+                            displayCurrent();
+
+                            // Call displayFiveDay method
+                            displayFiveDay(cursor);
+                        }
+                    } catch (JSONException e) {
+                        Log.e("JSON", e.toString());
+                    }
+                }
+            }
+        };
+        // Create Messenger Class
+        Messenger myMessenger = new Messenger(myHandler);
+
+        Intent intent = new Intent(_context,WeatherService.class);
+        intent.putExtra("messenger", myMessenger);
+        intent.putExtra("zip", field.getText().toString());
+
+        // Start Intent
+        startService(intent);
+    }
+
+
+    /* (non-Javadoc)
+     * @see com.jpennell.library.FormFragment.formListener#onSingleDaySelected(java.lang.Integer)
+     */
+    @SuppressWarnings("unchecked")
+	@Override
+    public void onSingleDaySelected(Integer i) {
+        Log.i("ROW CLICKED", "Row " + i + " clicked");
+
+        HashMap<String, String> detailMap = (HashMap<String, String>) _listView.getItemAtPosition(i);
+
+        Intent detailIntent = new Intent(_context, DetailsActivity.class);
+
+        String date = null;
+        String desc = null;
+        String hi = null;
+        String low = null;
+        String wind = null;
+
+        // Put extra
+        if (detailMap != null) {
+            date = detailMap.get("date");
+            desc = detailMap.get("desc");
+            hi = detailMap.get("hi");
+            low = detailMap.get("low");
+            wind = detailMap.get("wind");
+
+
+            detailIntent.putExtra("detailDate", date);
+            detailIntent.putExtra("detailDesc", desc);
+            detailIntent.putExtra("detailHi", hi);
+            detailIntent.putExtra("detailLow", low);
+            detailIntent.putExtra("detailWind", wind);
+
+            Log.i("INTENT", "Works");
+
+            //startActivityForResult(detailIntent, DAY_PICKED_REQUEST);
+        }
+
+        DetailFragment fragment = (DetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+        if (fragment != null && fragment.isInLayout()) {
+            fragment.displayDetails(date, desc, low, hi, wind);
+        } else {
+            // Start Activity for results
+            startActivityForResult(detailIntent, DAY_PICKED_REQUEST);
+        }
+
+
+    }
 }
